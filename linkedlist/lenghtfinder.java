2faf947fbd6d086f509ef14a8776ff2b78c14a8d
Jay <jpatel8110@gmail.com>
2018-01-19 00:54:45 -0800

    linked

---
+++ linkedlist/lenghtfinder.java
@@ -0,0 +1,51 @@
+package linkedlist;
+
+class Node
+     {
+        int data;
+        Node next;
+        Node(int d)
+         {
+             data=d;
+             next=null;
+         }
+    }
+public class lenghtfinder 
+{
+    Node head;
+    
+        public void push(int new_data)
+        {
+            Node new_node=new Node(new_data);
+            new_node.next=head;
+            head=new_node;
+        }
+        public int getcount()
+        {
+            Node temp=head;
+            int count=0;
+            while(temp!=null)
+            {
+              count++;
+             temp=temp.next;
+             
+           }
+            return count;
+            
+        }
+        
+        
+    
+    
+    
+    public static void main(String args[])
+    {
+        lenghtfinder fin=new lenghtfinder();
+        fin.push(1);
+        fin.push(2);
+        fin.push(3);
+        System.out.print("No of elements  "+fin.getcount());
+        
+    }
+
+}
